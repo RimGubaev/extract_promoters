16b594a56cfa5cf45835b98dd9a269367fe990ab
RimGubaev <rimgubaev@gmail.com>
2018-06-22 19:32:04 +0300

    Update gff2bed.r

--- gff2bed.r
+++ gff2bed.r
@@ -1,11 +1,14 @@
+#Set working directory
 wd <- getwd()
-
 setwd(wd)
 
+#Read gff file to be modified 
 GFF <- read.csv("genes.gff", sep = '\t', header = F)
 
+#Make bed file
 BED <- GFF[ ,c(1,4,5,9,8,7)]
 
+#Set the 
 BED$V8 <- 100
 
 BED$V9 <- gsub(".*GeneID:|;Name.*", "", BED$V9)
